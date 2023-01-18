30bd178dcfb76294c66f933ab5dcf27f4862202a
Aswanikp33 <120626544+Aswanikp33@users.noreply.github.com>
2023-01-18 14:47:27 +0530

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<stdlib.h>
+void push();
+void pop();
+void display();
+struct node
+{
+int data;
+struct node*next;
+};
+struct node *top=NULL;
+struct node *temp=NULL;
+void main()
+{
+int opt;
+do
+{
+printf("\nEnter the choice:\n1.push\n2.pop\n3.display\n4.exit");
+scanf("%d",&opt);
+switch(opt)
+{
+  case 1: push();
+	  break;
+  case 2: pop();
+	  break;
+  case 3: display();
+	  break;
+  case 4: exit(0);
+	  break;
+}
+}
+while(opt!=4);
+}
+void push()
+{
+struct node *new=NULL;
+new=(struct node*)malloc(sizeof (struct node));
+printf("Enter the data:");
+scanf("%d",&new->data);
+new->next=top;
+top=new;
+temp=top;
+}
+void pop()
+{
+struct node *ptr;
+if(top==NULL)
+printf("stack underflow");
+else
+{
+printf("The item should be poped %d",top->data);
+ptr=top;
+top=top->next;
+free(ptr);
+}
+}
+void display()
+{
+if(top==NULL)
+printf("stack underflow");
+else
+{
+while(top!=NULL)
+{
+printf("->%d",top->data);
+top=top->next;
+}
+}
+top=temp;
+}
+
+
+
