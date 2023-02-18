364767682971d3ecf9e545d4cc7008068ccb8808
Aswanikp33 <120626544+Aswanikp33@users.noreply.github.com>
2023-02-18 13:28:08 +0000

    Update and rename a2.c to merging.c

--- merging.c
+++ merging.c
@@ -1,17 +1,21 @@
 #include<stdio.h>
 void main()
 {
-int a[10],b[10],c[10],i,j,k,n,m,l,p;
+int a[10],b[10],c[10],i,j,k,n,m,l;
 printf("Enter the size of first array:");
 scanf("%d",&n);
 printf("Enter the elements:");
 for(i=0;i<n;i++)
+{
 scanf("%d",&a[i]);
+}
 printf("Enter the size of second array:");
 scanf("%d",&m);
 printf("Enter the elements:");
 for(j=0;j<m;j++)
+{
 scanf("%d",&b[j]);
+}
 printf("The resultant array is:\n");
 i=0;
 j=0;
@@ -31,16 +35,12 @@ j++;
 k++;
 }
 }
-if(i<n)
-{
 while (i<n)
 {
 c[k]=a[i];
 i++;
 k++;
 }
-}
-else if(j<m)
 {
 while(j<m)
 {
@@ -48,11 +48,10 @@ c[k]=b[j];
 j++;
 k++;
 }
-}
- p=m+n;
-for(k=0;k<p;k++)
+for(i=0;i<k;i++)
 {
-printf("%d\n",c[k]);
+printf("%d\n",c[i]);
+}
 }
 }
 
