cbf548d162e168c3fb9c746cb4ebf191f2a682c7
Aswanikp33 <120626544+Aswanikp33@users.noreply.github.com>
2023-02-21 09:23:42 +0000

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,72 +1,76 @@
 #include<stdio.h>
 #include<stdlib.h>
-void push();
-void pop();
-void display();
+ void push();
+ void pop();
+ void display();
 struct node
-{
-int data;
-struct node*next;
+{ 
+  int data;
+  struct node *next;
 };
-struct node *top=NULL;
-struct node *temp=NULL;
-void main()
+  struct node *top;
+  void main()
 {
-int opt;
-do
-{
-printf("\nEnter the choice:\n1.push\n2.pop\n3.display\n4.exit\n");
-scanf("%d",&opt);
-switch(opt)
-{
-  case 1: push();
-	  break;
-  case 2: pop();
-	  break;
-  case 3: display();
-	  break;
-  case 4: exit(0);
-	  break;
+ int opt;
+ do
+  {
+    printf("enter you option\n1.PUSH\n2.POP\n3.DISPLAY\n4.EXIT\n");
+    scanf("%d",&opt);
+    switch(opt)
+  {
+    case 1:push();
+           break;
+    case 2:pop();
+           break;
+    case 3:display();
+	   break;
+    case 4: exit(0);
+           break;
+    default:
+            printf("not valid\n");
+  } 
+  }while(opt!=4);
 }
-}
-while(opt!=4);
-}
+
 void push()
 {
-struct node *new=NULL;
-new=(struct node*)malloc(sizeof (struct node));
-printf("Enter the data:");
-scanf("%d",&new->data);
-new->next=top;
-top=new;
-temp=top;
+ struct node *new=NULL;
+ new=(struct node*)malloc(sizeof (struct node));
+ printf("enter the data to be pushed\n");
+ scanf("%d",&new->data);
+ new->next=top;
+ top=new;
 }
+ 
 void pop()
 {
-struct node *ptr;
-if(top==NULL)
-printf("stack underflow");
-else
-{
-printf("The item should be poped %d",top->data);
-ptr=top;
-top=top->next;
-free(ptr);
+ struct node *temp;
+ temp=top;
+ if (top==NULL)
+    printf("Stack underflow\n");
+ else
+   {
+    printf("the item to be poped\n%d",top->data);
+    
+    top=top->next;
+    free(temp);
+    }
 }
-}
+ 
 void display()
 {
-if(top==NULL)
-printf("stack underflow");
-else
-{
-while(top!=NULL)
-{
-printf("->%d",top->data);
-top=top->next;
-}
-}
-top=temp;
+ struct node *ptr;
+ ptr=top;
+ if(top==NULL)
+    printf("No List\n");  
+ else
+    {
+    while(ptr!=NULL)
+     {
+      printf("The list is %d\n",ptr->data);
+      ptr=ptr->next;
+     }
+    }
 }
 
 
